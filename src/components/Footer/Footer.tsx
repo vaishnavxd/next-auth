4fa4d3d485910d32342602504bc55f579c25b441
vaishnavxd <vaishnavvermaxd@gmail.com>
2025-02-16 14:45:11 +0530

    Add necessary components

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="border-t mt-auto">
+      <div className="max-w-7xl mx-auto px-4 py-4 text-center text-sm text-gray-600">
+        © {new Date().getFullYear()} {'{vaishnav}'} All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer; 
